22b7da1ef7d9080649e144be8d3c442735815d10
nikmul19 <nikmul19@gmail.com>
2020-04-23 16:04:19 +0530

    Updates

---
+++ precache-manifest.01b11199c4686d5fe86ee7d19ffb74b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099763cfaaee9527dbb44a960bcaca89",
+    "url": "/SpaceNos-Hiring/index.html"
+  },
+  {
+    "revision": "159d6e48e6e99a56b3aa",
+    "url": "/SpaceNos-Hiring/static/css/main.0affa77d.chunk.css"
+  },
+  {
+    "revision": "e3b25b0ff435eb24b97e",
+    "url": "/SpaceNos-Hiring/static/js/2.11669f40.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/SpaceNos-Hiring/static/js/2.11669f40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159d6e48e6e99a56b3aa",
+    "url": "/SpaceNos-Hiring/static/js/main.4135458f.chunk.js"
+  },
+  {
+    "revision": "7bc2ec46070416e3d781",
+    "url": "/SpaceNos-Hiring/static/js/runtime-main.9364ee98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SpaceNos-Hiring/static/media/logo.5d5d9eef.svg"
+  }
+]);
